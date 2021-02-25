72a9b7d70ebb824cf887e5f86ae41ea01dd013c3
Enormity Hacking <79352836+ZeusXalocofc@users.noreply.github.com>
2021-02-25 06:28:47 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+print('BEM VINDO AO MEU SCRIPT DE CONVERSA :)')
+print('github: https://github.com/ZeusXalocofc')
+import urllib.request
+import os
+import time
+import sys
+
+print ()
+
+nome = input ("digite seu nome:	")
+print()
+idade = input ("Olá, {}, 	Agora digite sua idade:	".format (nome) )
+print() 
+estuda = input (" Ok!, {},  O que Você Estuda?:	" .format (nome)  )
+print()
+cursou = input ("{},  Qual curso já Fez?:	 ".format (nome) )
+print()
+star = input ("Você Sabe O quê Sobre {}? ".format (cursou) )
+print()
+enjoy = input ("O que é {}?:	".format (cursou) )
+print()
+ok = input ("Uau, Muito bom Saber isso de {}!, O que Quer Ser Na Vida?:	".format (cursou) )
+print()
+perg = input ("O que te Leva á querer ser {}?:	".format (ok) )
+print()
+senti = input ("{},O que Você Gosta (Programação) ou (Hacking): 	".format (nome) )
+print()
+porq = input ("Nossa {}, Por quê?:	".format (senti) )
+
+
+print()
+
+
+print('Foi Bom Saber um Pouco de Você, Vou ser Atualizado em breve ;)')
+
+sys.exit()
